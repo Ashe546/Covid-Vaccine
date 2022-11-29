b3fe6d27931d58c2c5577eafc8ff04eeb9ec36cd
Ashenafi Endashaw <asfyd42@gmail.com>
2022-11-29 04:16:37 -0300

    Linter error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className='text-pink-600'>Hellow world</h1>
+      <h1 className="text-pink-600">Hellow world</h1>
     </div>
   );
 }
